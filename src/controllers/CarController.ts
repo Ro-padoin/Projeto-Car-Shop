054d7e29527765889dde72c2932304f58264a9b5
Rosalia Padoin Oliveira <koxelinho@icloud.com>
2022-07-29 19:19:06 -0300

    feat: requisito 8

--- src/controllers/CarController.ts
+++ src/controllers/CarController.ts
@@ -9,6 +9,11 @@ class CarController {
     const newCar = await this._service.create(req.body);
     return res.status(201).json(newCar);
   }
+
+  public async read(req: Request, res: Response<ICar[]>) {
+    const allCars = await this._service.read();
+    return res.status(200).json(allCars);
+  }
 }
 
 export default CarController;
---
+++ src/tests/mocks/car.mock.ts
@@ -0,0 +1,11 @@
+import { ICar } from '../../interfaces/ICar';
+
+export const carMock: ICar = {
+  model: "Bronco Sport",
+  year: 2023,
+  color: "Azul Malacara",
+  status: true,
+  buyValue: 267.000,
+  doorsQty: 4,
+  seatsQty: 5,
+}
