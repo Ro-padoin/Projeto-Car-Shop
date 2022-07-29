343e1e8d7520e4d1f4899ff7f23dda90bb26ed39
Rosalia Padoin Oliveira <koxelinho@icloud.com>
2022-07-29 18:43:31 -0300

    feat: requisito 4

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -3,7 +3,8 @@ import { z } from 'zod';
 const vehicleZodSchema = z.object({
   model: z.string().min(3),
   year: z.number().gte(1900).lte(2022),
-  color: z.string().min(3),
+  color: z.string()
+    .min(3),
   status: z.boolean().optional(),
   buyValue: z.number().int(),
 });
---
+++ src/services/CarService.ts
@@ -0,0 +1,52 @@
+import { IService } from '../interfaces/IService';
+import { ICar, carZodSchema } from '../interfaces/ICar';
+import { IModel } from '../interfaces/IModel';
+import { ErrorTypes } from '../errors/catalog';
+
+class CarService implements IService<ICar> {
+  private _car: IModel<ICar>;
+
+  constructor(model:IModel<ICar>) {
+    this._car = model;
+  }
+
+  public async create(obj: ICar): Promise<ICar> {
+    const parsed = carZodSchema.safeParse(obj);
+    if (!parsed.success) {
+      throw parsed.error;
+    }
+    return this._car.create(obj);
+  }
+
+  public async read(): Promise<ICar[]> {
+    const car = await this._car.read();
+    if (!car) {
+      throw new Error(ErrorTypes.EntityNotFound);
+    }
+    return car;
+  }
+
+  public async readOne(_id:string):Promise<ICar> {
+    const carById = await this._car.readOne(_id);
+    if (!carById) throw new Error(ErrorTypes.EntityNotFound);
+    return carById;
+  }
+
+  public async update(_id:string, obj:ICar):Promise<ICar | null> {
+    const parsed = carZodSchema.safeParse(obj);
+    if (!parsed.success) {
+      throw parsed.error; 
+    }
+    const carExists = await this.readOne(_id);
+    if (!carExists) throw new Error('Car not Exists');
+    return this._car.update(_id, obj);
+  }
+
+  public async delete(_id:string):Promise<ICar> {
+    const carDeleted = await this._car.delete(_id);
+    if (!carDeleted) throw new Error(ErrorTypes.EntityNotFound);
+    return carDeleted;
+  }
+}
+
+export default CarService;
